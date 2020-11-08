0d9c331f31fb8e1442ef81eede8b3b5d1c00fd23
David Talmage <talmage@acm.org>
2020-11-08 16:43:30 -0500

    This code reveals a byte-swapping bug in the radio send-receive library.

--- main.ts
+++ main.ts
@@ -1,5 +1,18 @@
+/**
+ * This code reveals a bug in the way that MakeCode sends numbers over the radio.
+ * 
+ * When we send an integer via the radio, it arrives with the bytes swapped.  E.g. 0xDEAD arrives as 0xDAED.
+ * 
+ * The micro:bit's Nordic nRF51822 is a little-endian CPU, so it stores 0xDEAD as 0xDAED.
+ * 
+ * One of two things is happening:
+ * 
+ * 1. The micro:bit sends in host order, converts from network order to host order on arrival: 0xDEAD.  Prints 0xDAED.
+ * 
+ * 2. The micro:bit sends in network order, leaves unchanged on arrival: 0xDEAD. Prints 0xDAED.
+ */
 input.onButtonPressed(Button.A, function () {
-    // Send 0xDEAD
+    // Send 0xDEAD.
     radio.sendValue("A", 57005)
 })
 input.onButtonPressed(Button.B, function () {
@@ -8,10 +21,10 @@ input.onButtonPressed(Button.B, function () {
 })
 radio.onReceivedValue(function (name, value) {
     if (name == "A") {
-        // Swaps bytes. Prints 0xDAED
+        // Prints 0xDAED
         basic.showString("A " + ("" + smarttools.dec2Hex(value)))
     } else if (name == "B") {
-        // Swaps bytes.  Prints 0xFEEB
+        // Prints 0xFEEB
         basic.showString("B " + ("" + smarttools.dec2Hex(value)))
     } else {
         basic.clearScreen()
